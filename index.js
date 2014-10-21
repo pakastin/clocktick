59121492fe79487535d9e5b939a3b4b7fb04e597
Juha Lindstedt <juha@pakastin.fi>
2014-10-21 15:32:41 +0300

    fixed arguments length

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ module.exports = clocktick
 
 function clocktick (callback, timeout) {
   var self, args, now, nowRounded, nextTick
-  var timeout = timeout || 1
+  var timeout = timeout * 1000 || 1000
 
   self = this
-  args = Array.prototype.slice.call(arguments, 1)
+  args = Array.prototype.slice.call(arguments, 2)
   now = dateNow()
-  nowRounded = (now / timeout*1000 | 0) * timeout*1000
-  nextTick = timeout*1000 - (now - nowRounded)
+  nowRounded = (now / timeout | 0) * timeout
+  nextTick = timeout - (now - nowRounded)
 
   return setTimeout(function () {
     callback && callback.apply(self, args)
