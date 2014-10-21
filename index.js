d395a3f11cd707d2f715a746c89f62eb14a9dd19
Juha Lindstedt <juha@pakastin.fi>
2014-10-21 15:19:44 +0300

    added optional parameter to define timeout time

--- index.js
+++ index.js
@@ -5,14 +5,15 @@ var dateNow = require('date-now')
 
 module.exports = clocktick
 
-function clocktick (callback) {
+function clocktick (callback, timeout) {
   var self, args, now, nowRounded, nextTick
+  var timeout = timeout || 1
 
   self = this
   args = Array.prototype.slice.call(arguments, 1)
   now = dateNow()
-  nowRounded = (now / 1000 | 0) * 1000
-  nextTick = 1000 - (now - nowRounded)
+  nowRounded = (now / timeout*1000 | 0) * timeout*1000
+  nextTick = timeout*1000 - (now - nowRounded)
 
   return setTimeout(function () {
     callback && callback.apply(self, args)
