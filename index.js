7c1e3e96e01cad61dede54bfab36e117bba73bc7
Juha Lindstedt <juha@pakastin.fi>
2014-11-20 05:31:41 +0200

    added offset value

--- index.js
+++ index.js
@@ -5,15 +5,16 @@ var dateNow = require('date-now')
 
 module.exports = clocktick
 
-function clocktick (callback, timeout) {
+function clocktick (callback, timeout, offset) {
   var self, args, now, nowRounded, nextTick
   var timeout = (timeout * 1000) || 1000
+  var offset = (offset * 1000) || 0
 
   self = this
   args = Array.prototype.slice.call(arguments, 2)
   now = dateNow()
   nowRounded = (now / timeout | 0) * timeout
-  nextTick = timeout - (now - nowRounded)
+  nextTick = timeout - (now - nowRounded) + offset
 
   return setTimeout(function () {
     callback && callback.apply(self, args)
